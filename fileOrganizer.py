cab44687050c8100f0cc9122bea56d9be858705d
Zid95 <zid.abdulkarim@gmail.com>
2023-03-02 23:39:31 +0100

    create function fileorganizer

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -7,4 +7,11 @@ files = os.listdir(path)
 
 
 for file in files:
-    pass
+    filename , extension = os.path.splitext(file)
+    extension = extension[1:]
+
+    if os.path.exists(path + "/" + extension):
+        shutil.move(path  + "/" + file,path + "/" + extension + "/" + file )
+    else:
+        os.makedirs(path + "/" + extension)
+        shutil.move(path + "/" + file,path + "/" + extension + "/" + file)
